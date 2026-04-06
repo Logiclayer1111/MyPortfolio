f33e5a20fa9e307ce4833c4b199c2b2889e73867
Antoni Nowicki <atn2122804@gmail.com>
2026-04-06 23:34:26 +0200

    Modified about.js, project.js and skills.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,13 +5,13 @@ const About = () => {
       className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
+        <div className="pb-8 text-center">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             About
           </p>
         </div>
 
-        <p className="text-xl mt-8 md:mt-20 px-4 md:px-0">
+        <p className="text-xl mt-8 md:mt-20 px-4 md:px-0 text-center">
           I'm a Full Stack Developer with 8 years of experience designing, building, and scaling modern web applications using advanced frontend and backend technologies. 
           Specialized in JavaScript ecosystems, scalable backend architectures, API design, and high-performance UI development across complex enterprise systems. 
           Demonstrates strong expertise in React, Vue.js, Node.js, TypeScript, RESTful APIs, cloud deployment, and database optimization, delivering secure and maintainable applications used by thousands of users.
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -45,7 +45,7 @@ const techs = [
   return (
     <div name="skills" className="bg-gradient-to-b from-gray-800 to-black w-full h-auto py-8">
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full text-white">
-        <div>
+        <div className="text-center">
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-5 inline">Skills</p>
           <p className="py-6 mt-5">These are the technologies I've worked with</p>
         </div>
